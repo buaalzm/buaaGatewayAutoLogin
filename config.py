396d68dca4ac1e8fa99095ea2b61352e67c1e172
李梓民 <243856864@qq.com>
2021-11-08 14:30:38 +0800

    add logger

--- config.py
+++ config.py
@@ -1,3 +1,29 @@
-gatewayUrl = "https://gw.buaa.edu.cn"
-USERNAME = 
-PASSWORD = 
+import socket
+
+# basic config
+
+USERNAME = ""
+PASSWORD = ""
+
+# advance config
+
+config={
+    "gatewayUrl" : "https://gw.buaa.edu.cn",
+    "USERNAME" : "",
+    "PASSWORD" : "",
+
+    "loki_config" : {
+            "url": "http://116.62.247.0:3100/loki/api/v1/push",
+            "tags": {
+                "application": "buaaGatewayAutoLogin",
+                "pc": socket.gethostname()
+            },
+            "version": "1"
+        }  # 日志相关
+}
+
+config["USERNAME"] = USERNAME
+config["PASSWORD"] = PASSWORD
+
+assert USERNAME != ""
+assert PASSWORD != ""
--- logintask.py
+++ logintask.py
@@ -1,20 +1,26 @@
 from loguru import logger
 from apscheduler.schedulers.blocking import BlockingScheduler
 from BeihangLogin.srun4k import *
+import logging_loki
+from loguru import logger
 from config import *
+
+
+logger.add(logging_loki.LokiHandler(**config["loki_config"]))
+logger.add("/log/{}.log".format(config["loki_config"]["tags"]["application"]))
 
 
 def login_task():
     logger.info("login")
-    ret = do_login(gatewayUrl, USERNAME, PASSWORD)
+    ret = do_login(config["gatewayUrl"], config["USERNAME"], config["PASSWORD"])
     if ret['success']:
-        logger.info('成功！')
+        logger.info('login success!')
     else:
-        logger.warning('失败！\n' + ret['reason'])
+        logger.warning('login failed\n' + ret['reason'])
 
 
 if __name__ == "__main__":
-    logger.add("/log/logintask.log")
+    logger.info("login bot init:config is {}".format(str(config)))
     login_task()
     sched = BlockingScheduler()
     sched.add_job(login_task, 'interval', hours=1 ,misfire_grace_time=10)
